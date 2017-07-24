a5efd0b66b63c4bc9d567b76071a305d209ef12d
tobiasmarke <tmarke@meteo.uni-koeln.de>
2017-07-24 16:40:46 +0200

    Update calcWindQuantities.m

--- calcWindQuantities.m
+++ calcWindQuantities.m
@@ -1,8 +1,8 @@
 function [time_o,beta,skewn,epsilon,epsilon_error,shear_vec,shear_dir,...
     aero_top,velo,velo_error,sigma_w,sigma_w_error,aero_layer_mask,...
-    nsamples] = calcWindQuantities(data_vert,data_wind_tday,...
+    nsamples,speed,direc,signal] = calcWindQuantities(data_vert,data_wind_tday,...
     data_wind_yday,data_wind_tmrw,model_wind_tday,model_wind_yday,...
-    model_wind_tmrw,dt)
+    model_wind_tmrw,dt,site,cut_h)
 %CALCWINDQUANTITIES calculates different quantities from Halo Doppler wind
 %lidar data
 %
@@ -14,7 +14,7 @@ function [time_o,beta,skewn,epsilon,epsilon_error,shear_vec,shear_dir,...
 %% Calculate TKE, velo, beta
 % Load vertical data and wind data from yesterday, today and tomorrow
 [epsilon,epsilon_error,t_epsilon,velo,velo_error,sigma_w,sigma_w_error,...
-    beta,~,~,nsamples] = calcTKE(data_vert,data_wind_tday,...
+    beta,~,~,nsamples,signal] = calcTKE(data_vert,data_wind_tday,...
     data_wind_yday,data_wind_tmrw,dt);
 % Fill missing lidar TKE with model TKE
 fnames = fieldnames(beta);
@@ -40,17 +40,17 @@ for it = 1:length(dt)
     aero_top_beta_it = nan(size(beta_it,1),1);
     aero_layer_mask_it = zeros(size(beta_it));
     for ip = 1:size(beta_it,1)
-        for jp = 4:size(beta_it,2)-6
+        for jp = cut_h+1:size(beta_it,2)-6
             itop_beta_it(ip,jp) = not((isnan(beta_it(ip,jp)) | beta_it(ip,jp) == 0) &...
                 (all(isnan(beta_it(ip,jp:jp+6))) | all(beta_it(ip,jp:jp+6)==0)));
         end
-        itop_beta_it(ip,[1:3,find(itop_beta_it(ip,:) == 0,1,'first'):end]) = 0;
-        tmp = itop_beta_it(ip,:); tmp(1:3) = 1;
+        itop_beta_it(ip,[1:cut_h,find(itop_beta_it(ip,:) == 0,1,'first'):end]) = 0;
+        tmp = itop_beta_it(ip,:); tmp(1:cut_h) = 1;
         aero_top_beta_it(ip) = find(tmp == 0,1,'first')-1;
         aero_layer_mask_it(ip,1:aero_top_beta_it(ip)) = 1;
     end
-    aero_layer_mask_it(:,1:3) = 0;
-    aero_top_beta_it(aero_top_beta_it<4) = nan;
+    aero_layer_mask_it(:,1:cut_h) = 0;
+    aero_top_beta_it(aero_top_beta_it<cut_h+1) = nan;
     aero_top_beta_it = round(smooth(inpaint_nans(aero_top_beta_it,4),5));
     aero_top_beta_it(all(isnan(beta_it),2)) = nan;
     if dt(it)==.5
@@ -66,9 +66,9 @@ end
 aatimes = struct2cell(t_epsilon);
 % dt = [2 3 5];
 for kk = 1:length(aatimes)
-    [Xr,Yr] = meshgrid(data_wind_tday.range, aatimes{kk});
+    [Xr,Yr] = meshgrid(data_vert.range, aatimes{kk});
     % Take day before and after to fill some gaps
-    if ~isempty(data_wind_tmrw)
+    if ~isempty(data_wind_tmrw) && ~isempty(data_wind_yday)
     [Xo,Yo] = meshgrid(data_wind_tday.range,...
         [data_wind_yday.time(:)-24;...
         data_wind_tday.time(:);...
@@ -85,20 +85,25 @@ for kk = 1:length(aatimes)
         [Xo,Yo] = meshgrid(data_wind_tday.range,data_wind_tday.time);
         uwind = interp2(Xo,Yo,data_wind_tday.uwind,Xr,Yr);
         vwind = interp2(Xo,Yo,data_wind_tday.vwind,Xr,Yr);
-    end
         
+    end
+    
+    speed_tmp = sqrt(uwind.^2+vwind.^2);  
+    direc_tmp = rad2deg(atan2(-uwind,-vwind));
+    direc_tmp(direc_tmp < 0) = direc_tmp(direc_tmp < 0)+360;
+    
     % Calculate wind shear
     win_size = 5;
     shear_dir_tmp = nan(size(uwind));
     shear_vec_tmp = nan(size(uwind));
     for ir = 1:length(aatimes{kk})
-        for ic = floor(win_size/2)+1:length(data_vert.range)-floor(win_size/2)
+        for ic = floor(win_size/2)+1:length(data_wind_tday.range)-floor(win_size/2)
             duwind = uwind(ir,ic+floor(win_size/2))-...
                 uwind(ir,ic-floor(win_size/2));
             dvwind = vwind(ir,ic+floor(win_size/2))-...
                 vwind(ir,ic-floor(win_size/2));
-            dz = data_vert.range(ic + floor(win_size/2))-...
-                data_vert.range(ic - floor(win_size/2));
+            dz = data_wind_tday.range(ic + floor(win_size/2))-...
+                data_wind_tday.range(ic - floor(win_size/2));
             shear_vec_tmp(ir,ic) = sqrt((duwind).^2 + (dvwind).^2) ./ dz;
             shear_dir_tmp(ir,ic) = 180 * atan2(dvwind,duwind) / pi;
         end
@@ -120,9 +125,13 @@ for kk = 1:length(aatimes)
     if dt(kk)==.5
         shear_vec.t_0_5min = shear_vec_tmp;
         shear_dir.t_0_5min = shear_dir_tmp;
+        speed.t_0_5min = speed_tmp;
+        direc.t_0_5min = direc_tmp;
     else
         eval(['shear_vec.t_' sprintf('%s',num2str(dt(kk))) 'min = shear_vec_tmp;'])
         eval(['shear_dir.t_' sprintf('%s',num2str(dt(kk))) 'min = shear_dir_tmp;'])
+        eval(['speed.t_' sprintf('%s',num2str(dt(kk))) 'min = speed_tmp;'])
+        eval(['direc.t_' sprintf('%s',num2str(dt(kk))) 'min = direc_tmp;'])
     end
 end
 
@@ -145,12 +154,16 @@ for ifn = 1:length(fnames)
     skewn.(fnames{ifn}) = interp2(Xo,Yo,skewn_tmp,Xr,Yr);
     skewn.(fnames{ifn})(isnan(beta.(fnames{ifn}))) = nan;
     
-    % clean 3 lowest most range gates and skewness
-    beta.(fnames{ifn})(:,1:3) = nan;
-    velo.(fnames{ifn})(:,1:3) = nan;
-    epsilon.(fnames{ifn})(:,1:3) = nan;
-    skewn.(fnames{ifn})(:,1:3) = nan;
-    sigma_w.(fnames{ifn})(:,1:3) = nan;
+    % clean cut_h lowest most range gates
+    beta.(fnames{ifn})(:,1:cut_h) = nan;
+    velo.(fnames{ifn})(:,1:cut_h) = nan;
+    epsilon.(fnames{ifn})(:,1:cut_h) = nan;
+    skewn.(fnames{ifn})(:,1:cut_h) = nan;
+    sigma_w.(fnames{ifn})(:,1:cut_h) = nan;
+    shear_vec.(fnames{ifn})(:,1:cut_h) = nan;
+    shear_dir.(fnames{ifn})(:,1:cut_h) = nan;
+    speed.(fnames{ifn})(:,1:cut_h) = nan;
+    direc.(fnames{ifn})(:,1:cut_h) = nan;
 end
 %% Time out
 time_o = t_epsilon;
